de4e20cee332e305a62683c943273b7c9a096cb0
HDunlop <dunlop.h@etoncollege.org.uk>
2020-07-26 17:26:16 +0800

    tutorial.cs v0

---
+++ tutorial.cs
@@ -0,0 +1,14 @@
+class Program
+{
+  static void Main(string[] args)
+  {
+    // print
+    Console.WriteLine("Hellow World!");
+
+    // input
+    Console.ReadLine("type smthing...\n");
+
+    // variables
+    string exampleVar = "e.g.";
+  }
+}
